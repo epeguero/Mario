792ed38e8b27b043da7fa7677ef314f8249413b6
epeguero <edwinmpeguero@gmail.com>
2016-05-24 18:08:21 -0400

    added cabal sandbox and cabal build system support. Separated main and game specific modules.

--- main.hs
+++ main.hs
@@ -1,21 +1,13 @@
+module Main(main) where
+
+import Game
+
 import Control.Monad
 import Control.Concurrent.MVar
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TQueue
 import Control.Concurrent
-import Data.Maybe
 import System.IO
-import qualified Data.Map.Strict as M
-
-type World = Int
-data State = InWorld | Exit deriving (Show)
-data Game = Game {world :: World, state :: State} deriving (Show)
-data Event = WorldEvent (World -> World) | StateEvent (State -> State)
-type Epoch = [Event]
-type UserInput = Char
-
-initialGame :: Game
-initialGame = Game {world = 0, state = InWorld}
 
 main :: IO ()
 main = do
@@ -42,17 +34,12 @@ main = do
         generateUserEpoch :: IO ()
         generateUserEpoch = do 
             game        <- readGame
-            input       <- readInput
-            case M.lookup input $ keyBindings game of
-                Nothing     -> return ()
-                Just event  -> writeEpoch [event]
-            generateUserEpoch 
-            
-        updateGameUI :: Game -> IO()
-        updateGameUI = print . world
+            input       <- readUserInput
+            writeEpoch $ userEpoch game input
+            generateUserEpoch
 
-        readInput :: IO (UserInput)
-        readInput = getChar
+        readUserInput :: IO (UserInput)
+        readUserInput = getChar
 
         readGame :: IO (Game)
         readGame = readMVar gameMVar
@@ -72,22 +59,6 @@ main = do
             forkIO  $ generateWorldEpoch
             forever $ generateUserEpoch
 
-keyBindings :: Game -> M.Map UserInput Event
-keyBindings (Game {state = Exit}) = M.fromList []
-keyBindings (Game {state = InWorld}) = M.fromList 
-            [   ('a',WorldEvent (+1)) , 
-                ('r', WorldEvent (const 0)), 
-                ('q', StateEvent (const Exit))]
-
-gameWorldEpoch :: Game -> Epoch
-gameWorldEpoch game = 
-    foldl (\accu (pred,e) -> if pred game then e:accu else accu) [] 
-    [((< 20) . world, WorldEvent $ (subtract 1))]
-
-applyEvent :: Game -> Event -> Game
-applyEvent game@(Game {world = _, state = Exit}) _ = game
-applyEvent game (WorldEvent e) = game {world = e . world $ game}
-applyEvent game (StateEvent e) = game {state = e . state $ game} 
 
 
 
